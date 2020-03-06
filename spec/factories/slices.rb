5dad3075fe72d16dcfbfee816babf9c2aca3daa1
joaofelipe <joaofelipe1294@gmail.com>
2020-03-06 13:51:38 +0000

    Regenerated model slice

--- spec/factories/slices.rb
+++ spec/factories/slices.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
   factory :slice do
     responsible_id { 1 }
     responsible_login { "MyString" }
-    date { "2020-03-05" }
+    date { "2020-03-06" }
     subsample_id { 1 }
     subsample_label { "MyString" }
+    probe { 1 }
   end
 end
