1cdff2f22be2dfe04b5fb277b88cc9ccc3ddf46c
joaofelipe <joaofelipe1294@gmail.com>
2020-03-06 14:49:41 +0000

    Add slices controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  resources :slices
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/helpers/slices_helper_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe SlicesHelper, type: :helper do
+end
