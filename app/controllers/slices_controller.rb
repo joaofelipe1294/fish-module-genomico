d353a3f64b38e06ca9c5031aca05ba644a9ecc70
joaofelipe <joaofelipe1294@gmail.com>
2020-03-06 17:26:09 +0000

    Added slice base form

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -1,8 +1,11 @@
 class SlicesController < ApplicationController
-  def new
-  end
+  helper_method :users
 
   def index
+  end
+
+  def new
+    @slice = Slice.new({subsample_id: params[:subsample_id], subsample_label: params[:subsample_label]})
   end
 
   def edit
@@ -10,4 +13,11 @@ class SlicesController < ApplicationController
 
   def show
   end
+
+  def users
+    response = RestClient.get('cda0015.cdapp.net.br:3000/fish-api/users')
+    response = JSON.parse response
+    response.map { |user| User.new(id: user["id"], login: user["login"])}
+  end
+
 end
