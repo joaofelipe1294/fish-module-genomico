177cc5c2808906f9f7211530dd8c16d1969040cd
joaofelipe <joaofelipe1294@gmail.com>
2020-03-06 18:19:10 +0000

    Added method thad create a slice.

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -8,6 +8,16 @@ class SlicesController < ApplicationController
     @slice = Slice.new({subsample_id: params[:subsample_id], subsample_label: params[:subsample_label]})
   end
 
+  def create
+    @slice = Slice.new slice_params
+    if @slice.save
+      flash[:success] = I18n.t :new_slice_success
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
@@ -19,5 +29,11 @@ class SlicesController < ApplicationController
     response = JSON.parse response
     response.map { |user| User.new(id: user["id"], login: user["login"])}
   end
+
+  private
+
+    def slice_params
+      params.require(:slice).permit(:responsible_id, :responsible_login, :date, :subsample_id, :subsample_label, :probe)
+    end
 
 end
