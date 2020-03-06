36a5abc05a6ae5cc991bb40f2c8915a25d91946d
joaofelipe <joaofelipe1294@gmail.com>
2020-03-06 12:29:21 +0000

    Getting data from genomico/fish-app/exams ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+
   def index
+    response = RestClient.get('cda0015.cdapp.net.br:3000/fish-api/exams')
+    @exams = JSON.parse response
   end
+
 end
