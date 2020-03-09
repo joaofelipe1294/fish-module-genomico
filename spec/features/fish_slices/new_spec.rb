86b98b9f40d970d1ecc1199a1508f1b71b305c22
joaofelipe <joaofelipe1294@gmail.com>
2020-03-09 19:22:07 +0000

    Changed slice to fish_slice

--- spec/features/fish_slices/new_spec.rb
+++ spec/features/fish_slices/new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Slices::News", type: :feature do
+RSpec.feature "FishSlices::News", type: :feature do
 
   before(:each) { visit root_path }
 
@@ -8,7 +8,7 @@ RSpec.feature "Slices::News", type: :feature do
     context "when user click in new-slice link" do
       it 'is expected to redirect to new_slice_path' do
         click_link class: 'new-slice', match: :first
-        expect(page).to have_current_path(new_slice_path, ignore_query: true)
+        expect(page).to have_current_path(new_fish_slice_path, ignore_query: true)
       end
     end
     context "when in new slice view" do
@@ -17,14 +17,14 @@ RSpec.feature "Slices::News", type: :feature do
       end
       context "when fill all values", js: true do
         before :each do
-          fill_in "slice[date]", with: Date.current
+          fill_in "fish_slice[date]", with: Date.current
           click_button id: 'btn-save'
         end
         it 'is expected to render success' do
-          expect(find(id: 'success-message').text).to match I18n.t :new_slice_success
+          expect(find(id: 'success-message').text).to match I18n.t :new_fish_slice_success
         end
         it 'is expected to create slice' do
-          expect(Slice.all.empty?).to be_falsey
+          expect(FishSlice.all.empty?).to be_falsey
         end
         it 'is expected to be redirected to root_path' do # by now
           expect(page).to have_current_path root_path
