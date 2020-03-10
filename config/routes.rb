834159209936d4b10bcb5581b7eaaf80c525a7b6
joaofelipe <joaofelipe1294@gmail.com>
2020-03-10 14:09:38 +0000

    Added link that rendirect to new-capture

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'scannings/index'
-  get 'scannings/new'
+  resources :scannings
   resources :fish_slices
   get 'exam/:id/slices', to: 'fish_slices#slices_from_exam', as: :slices_from_exam
   root to: 'home#index'
