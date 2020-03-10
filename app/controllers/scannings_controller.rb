c8de27dc417321ed18e2e74941efc67a73b4e832
joaofelipe <joaofelipe1294@gmail.com>
2020-03-10 14:47:22 +0000

    Implemented scannings_controller#create

--- app/controllers/scannings_controller.rb
+++ app/controllers/scannings_controller.rb
@@ -7,4 +7,26 @@ class ScanningsController < ApplicationController
     @scanning = Scanning.new(fish_slice_id: params[:slice_id])
   end
 
+  def create
+    @scanning = Scanning.new scanning_params
+    if @scanning.save
+      flash[:success] = I18n.t :new_scanning_success
+      redirect_to scannings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def scanning_params
+      params.require(:scanning).permit(
+        :responsible_id,
+        :responsible_login,
+        :date,
+        :fish_slice_id,
+        images: []
+      )
+    end
+
 end
--- app/models/scanning.rb
+++ app/models/scanning.rb
@@ -2,5 +2,5 @@ class Scanning < ApplicationRecord
   include ResponsibleReference
   belongs_to :fish_slice
   validates_presence_of :date
-  has_many_attached :image
+  has_many_attached :images
 end
