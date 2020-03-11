311e0a03a91e80b1cd8b9ae202a90f77a940e400
joaofelipe <joaofelipe1294@gmail.com>
2020-03-11 14:08:21 +0000

    Implemented method that process images after saving a scanning.

--- app/models/scanning.rb
+++ app/models/scanning.rb
@@ -2,5 +2,19 @@ class Scanning < ApplicationRecord
   include ResponsibleReference
   belongs_to :fish_slice
   validates_presence_of :date
+  has_many :scanning_images
+  after_create_commit :generate_images
   has_many_attached :images
+
+  private
+
+    def generate_images
+      puts "----------------------- GENERATE IMAGES --------------------------"
+      self.images.each do |image|
+        ImageProcessorService.new({image: image, scanning: self}).call
+        # puts "PENDEJADA !!!"
+      end
+      puts "--------------------- END GENERATE IMAGES ------------------------"
+    end
+
 end
