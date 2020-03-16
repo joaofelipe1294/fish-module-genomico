44f3dd0a27adf80450d8deacb3c9f1f05ab33d86
joaofelipe <joaofelipe1294@gmail.com>
2020-03-16 13:30:43 +0000

    Added channels combinatiosn extraction.

--- app/models/scanned_cell.rb
+++ app/models/scanned_cell.rb
@@ -1,16 +1,44 @@
 class ScannedCell < ApplicationRecord
   belongs_to :scanning_image
   before_validation :set_label
-  has_one_attached :image
+  has_many_attached :images
   enum label: {
     positive: 1,
     negative: 0
   }
 
+  def rgb
+    find_image "nucleus"
+  end
+
+  def blue
+    find_image "blue_nucleus"
+  end
+
+  def green
+    find_image "green_nucleus"
+  end
+
+  def red
+    find_image "red_nucleus"
+  end
+
+  def red_green
+    find_image "red"
+  end
+
   private
 
     def set_label
       self.label = :positive unless self.label
+    end
+
+    def find_image channel
+      channel_image = nil
+      self.images.each do |image|
+        channel_image = image if image.filename.to_s == "#{channel}.png"
+      end
+      channel_image
     end
 
 end
