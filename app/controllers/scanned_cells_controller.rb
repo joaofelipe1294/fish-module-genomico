a2a48be8d66ae40d7c972e4ebe65806e14c04386
joaofelipe <joaofelipe1294@gmail.com>
2020-03-20 14:03:33 +0000

    Finished web service that extract image collor channels

--- app/controllers/scanned_cells_controller.rb
+++ app/controllers/scanned_cells_controller.rb
@@ -7,9 +7,10 @@ class ScannedCellsController < ApplicationController
       blue: cell.blue_path,
       green: cell.green_path,
       red: cell.red_path,
-      blue_green: cell.blue_red_path,
+      blue_green: cell.blue_green_path,
       blue_red: cell.blue_red_path,
-      green_red: cell.green_red_path
+      green_red: cell.green_red_path,
+      rgb: cell.rgb_path
     }
     render json: data, status: :ok
   end
--- app/models/scanned_cell.rb
+++ app/models/scanned_cell.rb
@@ -1,4 +1,5 @@
 class ScannedCell < ApplicationRecord
+  include Rails.application.routes.url_helpers
   belongs_to :scanning_image
   before_validation :set_label
   before_validation :set_process_status
@@ -21,28 +22,31 @@ class ScannedCell < ApplicationRecord
   }
 
   def blue_path
-    ActiveStorage::Blob.service.send(:path_for, self.blue.key)
+    get_attached_path self.blue
   end
 
   def green_path
-    ActiveStorage::Blob.service.send(:path_for, self.green.key)
+    get_attached_path self.green
   end
 
   def red_path
-    ActiveStorage::Blob.service.send(:path_for, self.red.key)
+    get_attached_path self.red
   end
 
   def blue_green_path
-    ActiveStorage::Blob.service.send(:path_for, self.blue_green.key)
+    get_attached_path self.blue_green
   end
 
-
   def blue_red_path
-    ActiveStorage::Blob.service.send(:path_for, self.blue_red.key)
+    get_attached_path self.blue_red
   end
 
   def green_red_path
-    ActiveStorage::Blob.service.send(:path_for, self.green_red.key)
+    get_attached_path self.green_red
+  end
+
+  def rgb_path
+    get_attached_path self.rgb
   end
 
   private
@@ -55,18 +59,21 @@ class ScannedCell < ApplicationRecord
       self.process_status = :waiting_start unless self.process_status
     end
 
-    def find_image channel
-      channel_image = nil
-      self.images.each do |image|
-        channel_image = image if image.filename.to_s == "#{channel}.png"
-      end
-      channel_image
+    def get_attached_path attachment
+      rails_blob_path(attachment, disposition: "attachment", only_path: true)
     end
 
+
+    # def find_image channel
+    #   channel_image = nil
+    #   self.images.each do |image|
+    #     channel_image = image if image.filename.to_s == "#{channel}.png"
+    #   end
+    #   channel_image
+    # end
     # def extract_color_channels_job
     #   NucleusChannelExtractionJob.perform_later(self)
     # end
-
     # def check_if_all_nucleus_are_processed
     #   scanning_image = self.scanning_image
     #   if scanning_image.scanned_cells.complete.size > scanning_image.scanned_cells.size * 0.75
