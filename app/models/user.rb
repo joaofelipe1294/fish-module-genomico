bf688af5581cb46f5fb0d1670b8d9297b910ff79
joaofelipe <joaofelipe1294@gmail.com>
2020-03-21 13:38:18 +0000

    Fixed workaround that return users in development env to work in test env

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User
       response = RestClient.get('http://cda0015.cdapp.net.br:3000/fish-api/users')
     rescue
       data = [{"id": 2, "login": 'azuka'}].to_json
-      response = data if Rails.env == "development"
+      response = data if Rails.env == "development" or Rails.env == "test"
     end
     response = JSON.parse response
     response.map { |user| User.new(id: user["id"], login: user["login"])}
