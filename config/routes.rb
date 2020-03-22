2af99cb131d877a64854f81a7d4ae32c9da70d4b
joaofelipe <joaofelipe1294@gmail.com>
2020-03-22 00:23:26 +0000

    Finished display of analyzed images

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   resources :scannings
   resources :fish_slices
   resources :scanned_cells, only: [:show]
-  patch 'scanning_images/:id/analyzed', to: 'scanning_images#analyzed', as: :analyze_scanning_image
+  patch 'scanning_images/:id/analyzed', to: 'scanning_images#change_status_to_analyzed', as: :change_status_to_analyzed
   patch 'scanned_cells/:id/change_label', to: 'scanned_cells#change_label', as: :change_cell_label
   get 'scanned_cells/treat/:id', to: 'scanned_cells#treat', as: :treat_scanned_cell
   get 'scanning-images/processing-progress', to: 'scanning_images#processing_progress', as: :processing_progress
-  get 'scanning-images/pending-analysis', to: 'scanning_images#pending_analysis', as: :pending_analysis
   get 'fish-slice/:id/scannings', to: 'fish_slices#scannings', as: :scannings_from_fish_slice
   get 'exam/:id/slices', to: 'fish_slices#slices_from_exam', as: :slices_from_exam
   get 'scanning-images/processing', to: 'scanning_images#processing', as: :processing
