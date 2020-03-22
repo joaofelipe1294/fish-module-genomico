511e724c7be42136bc496809184c1d2cd6576eb7
joaofelipe <joaofelipe1294@gmail.com>
2020-03-22 00:38:00 +0000

    Added script that install ruby dependancies

---
+++ vagrant_files/shell_scripts/ruby_libs.sh
@@ -0,0 +1,4 @@
+sudo apt install -y autoconf bison build-essential libssl1.0-dev \
+                    libyaml-dev libreadline-dev zlib1g-dev \
+                    libncurses5-dev libffi-dev libgdbm-dev \
+                    build-essential tcl
