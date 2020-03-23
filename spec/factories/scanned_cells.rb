b544dc51e8c9b5c86f2d95d873e65710b6f10e1c
joaofelipe <joaofelipe1294@gmail.com>
2020-03-23 17:55:58 +0000

    Added to scanned_cell factory rgb attached image

--- spec/factories/scanned_cells.rb
+++ spec/factories/scanned_cells.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :scanned_cell do
     scanning_image { ScanningImage.last }
     label { :positive }
+    rgb { Rack::Test::UploadedFile.new("#{Rails.root}/spec/support_images/NUCLEUS.png", 'image/png') }
   end
 end
