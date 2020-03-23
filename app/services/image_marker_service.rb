90f46a5f9a0ff0a9854e52b4f60149b965926908
joaofelipe <joaofelipe1294@gmail.com>
2020-03-23 18:38:18 +0000

    Added close and purge of files loded during image_marker_service

--- app/services/image_marker_service.rb
+++ app/services/image_marker_service.rb
@@ -22,12 +22,25 @@ class ImageMarkerService
     end
 
     def setup_scanning_image
-      original =  File.open(@image_data["original"])
-      marked =  File.open(@image_data["marked"])
+      load_files
       scanning_image = ScanningImage.new(scanning: @scanning, start_processing_at: DateTime.current, process_status: :marking_image)
       scanning_image.original.attach(io: original, filename: 'original-png.png', content_type: 'image/png')
       scanning_image.marked.attach(io: marked, filename: 'marked.png', content_type: 'image/png')
+      scanning_image.save
+      close_files
       scanning_image
+    end
+
+    def load_files
+      @original =  File.open(@image_data["original"])
+      @marked =  File.open(@image_data["marked"])
+    end
+
+    def close_files
+      @original.close
+      @marked.close
+      File.delete(@image_data["original"])
+      File.delete(@image_data["marked"])
     end
 
 end
