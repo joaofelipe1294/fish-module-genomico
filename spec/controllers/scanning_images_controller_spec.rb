2db1c95afcae31879a6e9d82cfb59177bbec4537
joaofelipe <joaofelipe1294@gmail.com>
2020-03-23 13:22:50 +0000

    Added tests that validates scanning_images controller json return.

--- spec/controllers/scanning_images_controller_spec.rb
+++ spec/controllers/scanning_images_controller_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
+require 'json'
 
 RSpec.describe ScanningImagesController, type: :controller do
+
+  describe "scanning_images?status='processing'" do
+    context "when there are images processing" do
+      before :each do
+        create(:fish_slice)
+        create(:scanning)
+        5.times { create(:scanning_image, start_processing_at: DateTime.current) }
+        ScanningImage.last.update process_status: :complete
+      end
+      it 'is expected to return ok status' do
+        request.env['HTTP_ACCEPT'] = 'application/json'
+        get :index, params: {status: 'processing'}
+        expect(response).to have_http_status :ok
+      end
+      it 'is expected to return scanning images list' do
+        request.env['HTTP_ACCEPT'] = 'application/json'
+        get :index, params: {status: 'processing'}
+        expect(JSON.parse(response.body).size).to eq 4
+      end
+    end
+    context "when there is no processing images" do
+      it 'is expected to return an empty array' do
+        request.env['HTTP_ACCEPT'] = 'application/json'
+        get :index, params: { status: 'processing' }
+        expect(JSON.parse(response.body).empty?).to be_truthy
+      end
+    end
+  end
+
+
 end
