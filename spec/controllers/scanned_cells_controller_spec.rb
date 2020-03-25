4066c3a37496559dc9abe06176c8c7f2f6a92f1c
joaofelipe <joaofelipe1294@gmail.com>
2020-03-25 13:18:46 +0000

    Add scanned_cells_controller#show spec

--- spec/controllers/scanned_cells_controller_spec.rb
+++ spec/controllers/scanned_cells_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'json'
 
 RSpec.describe ScannedCellsController, type: :controller do
 
@@ -37,6 +38,46 @@ RSpec.describe ScannedCellsController, type: :controller do
       end
     end
 
+  end
+
+  describe 'ScannedCell#show' do
+    context "when rendering rgb cell" do
+      before :each do
+        get :show, params: { id: @scanned_cell.id }
+      end
+      it 'is expected to render ok status' do
+        expect(response).to have_http_status :ok
+      end
+      it 'is expected to render paths to rgb image and other channels' do
+        json_response = JSON.parse response.body
+        expect(json_response.has_key?("blue")).to be_truthy
+        expect(json_response.has_key?("green")).to be_truthy
+        expect(json_response.has_key?("red")).to be_truthy
+        expect(json_response.has_key?("blue_green")).to be_truthy
+        expect(json_response.has_key?("blue_red")).to be_truthy
+        expect(json_response.has_key?("green_red")).to be_truthy
+        expect(json_response.has_key?("rgb")).to be_truthy
+      end
+
+    end
+    context "when rendering a treated image" do
+      before :each do
+        get :show, params: {id: @scanned_cell.id, treat: true}
+      end
+      it 'is expected to retun ok status' do
+        expect(response).to have_http_status :ok
+      end
+      it 'is expected to render paths to treated and rgb channels' do
+        json_response = JSON.parse response.body
+        expect(json_response.has_key?("blue")).to be_truthy
+        expect(json_response.has_key?("green")).to be_truthy
+        expect(json_response.has_key?("red")).to be_truthy
+        expect(json_response.has_key?("blue_green")).to be_truthy
+        expect(json_response.has_key?("blue_red")).to be_truthy
+        expect(json_response.has_key?("green_red")).to be_truthy
+        expect(json_response.has_key?("treated")).to be_truthy
+      end
+    end
   end
 
 end
